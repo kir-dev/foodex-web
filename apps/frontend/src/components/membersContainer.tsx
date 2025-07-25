083258431b0c197d90c4fa512b012cd987e682da
Sali Nóra <salinora2005@gmail.com>
2025-07-25 17:02:25 +0200

    Key-ként no index fix

--- apps/frontend/src/components/membersContainer.tsx
+++ apps/frontend/src/components/membersContainer.tsx
@@ -11,9 +11,9 @@ export function MembersContainer({ members }: Props) {
   return (
     <div className='w-full max-w-6xl rounded-xl p-2 mb-4'>
       <div className='flex flex-col gap-2 max-h-32 overflow-y-auto pr-1'>
-        {members.map((member, index) => (
+        {members.map((member) => (
           <div
-            key={index}
+            key={member.name}
             className='border-2 border-[#332C81] bg-[#332C81] rounded-xl p-2 flex items-center gap-2 text-white text-lg'
           >
             <span className='font-semibold text-[#FF9860]'>{member.name}</span>
--- apps/frontend/src/components/openingsContainer.tsx
+++ apps/frontend/src/components/openingsContainer.tsx
@@ -13,9 +13,9 @@ export function OpeningsContainer({ openings }: OpeningsProps) {
   return (
     <div className='w-full max-w-6xl rounded-xl p-2 mb-4'>
       <div className='flex flex-col gap-1 pr-1 max-h-32 overflow-y-auto'>
-        {openings.map((opening, index) => (
+        {openings.map((opening) => (
           <div
-            key={index}
+            key={opening.groupName}
             className='border-2 border-[#332C81] bg-[#332C81] rounded-xl px-3 py-2 grid sm:grid-cols-[minmax(6rem,auto)_minmax(6rem,auto)_minmax(6rem,auto)_1fr] gap-x-3 items-center text-white text-lg'
           >
             <span className='font-semibold'>{opening.groupName}</span>
