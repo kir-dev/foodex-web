3c33220998d6ef9d4ba4c9d6f9acd552a20fdd4b
MiklosBacsi <mbacsim@gmail.com>
2026-05-08 17:29:40 +0200

    Sync UserRepository

--- apps/backend/src/main/kotlin/hu/kirdev/foodex/user/UserRepository.kt
+++ apps/backend/src/main/kotlin/hu/kirdev/foodex/user/UserRepository.kt
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository
 
 @Repository
 interface UserRepository : JpaRepository<UserEntity, Int> {
+    fun findUserEntityByInternalId(internalId: String): UserEntity?
     fun findUserEntityByNameOrNicknameIgnoreCase(name: String, nickname: String): UserEntity?
     fun findUserEntitiesByNameOrNicknameIgnoreCase(name: String, nickname: String): List<UserEntity>
     fun findUserEntitiesByIsActiveTrue(): List<UserEntity>
