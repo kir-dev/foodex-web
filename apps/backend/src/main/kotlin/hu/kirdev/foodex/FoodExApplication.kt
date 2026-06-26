d04cf227119fde4027e24ecbfa14b181ea3c2cbc
MiklosBacsi <mbacsim@gmail.com>
2026-06-26 07:23:41 +0200

    Fix test data config

--- apps/backend/src/main/kotlin/hu/kirdev/foodex/FoodExApplication.kt
+++ apps/backend/src/main/kotlin/hu/kirdev/foodex/FoodExApplication.kt
@@ -1,25 +1,11 @@
 package hu.kirdev.foodex
 
-import hu.kirdev.foodex.testconfig.CookingClubTestConfig
-import hu.kirdev.foodex.testconfig.OpeningRequestTestConfig
-import hu.kirdev.foodex.testconfig.ShiftTestConfig
-import hu.kirdev.foodex.testconfig.UserTestConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Import
 
 @SpringBootApplication
-@Import(
-    UserTestConfig::class,
-    ShiftTestConfig::class,
-    OpeningRequestTestConfig::class,
-    CookingClubTestConfig::class,
-)
 class FoodExApplication
 
 fun main(args: Array<String>) {
-    val profiles = arrayOf("test")
-	runApplication<FoodExApplication>(*args) {
-        setAdditionalProfiles(*profiles)
-    }
+	runApplication<FoodExApplication>(*args)
 }
